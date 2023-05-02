656799bc76c59ede028c02caed452684c6d45e43
Artur-JS <info.arahmanov@gmail.com>
2023-05-02 14:22:47 +0500

    Origin Landing Mock

---
+++ src/components/SendData.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './senddata.css'
+
+const SendData = () => {
+
+  return (
+    <div className='main__page'>
+        <div className='main__page_title'>
+            <h1>Конструктор интеграций от LEADTEX</h1>
+            <h2>Скоро запуск. Следи за проектом.</h2>
+        </div>
+        <div className="footer">Лето 2023</div>
+    </div>
+  )
+}
+
+export default SendData
