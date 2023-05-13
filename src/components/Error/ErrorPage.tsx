73cd3493681b85dc51b516f6a7e222bbb3db4a28
Artur-JS <info.arahmanov@gmail.com>
2023-05-14 00:37:53 +0500

    responsive_add_changes

--- src/components/Error/ErrorPage.tsx
+++ src/components/Error/ErrorPage.tsx
@@ -3,16 +3,16 @@ import './error.css'
 
 const ErrorPage = () => {
   return (
-    <div className='main__page'>
-        <div className='main__page_title'>
+    <div className='error__page'>
+        <div className='error__page_title'>
             <h1>Конструктор интеграций от LEADTEX</h1>
             <h2>Скоро запуск. Следи за проектом.</h2>
         </div>
-        <div className='info'>
+        <div className='error_info'>
             <h3>Что-то пошло не так</h3>
             <h4>Повторите попытку позже или проверьте интернет соединение.</h4>
         </div>
-        <div className="footer">Лето 2023</div>
+        <div className="error_footer">Лето 2023</div>
     </div>
   )
 }
--- src/components/SuccessForm/SuccessForm.tsx
+++ src/components/SuccessForm/SuccessForm.tsx
@@ -6,12 +6,12 @@ import './success.css'
 const SuccessForm = () => {
 
   return (
-    <div className='main__page'>
-        <div className='main__page_title'>
+    <div className='success__page'>
+        <div className='success__page_title'>
             <h1>Конструктор интеграций от LEADTEX</h1>
             <h2>Скоро запуск. Следи за проектом.</h2>
         </div>
-        <div className='info'>
+        <div className='success_info'>
             <h3>Спасибо за Ваш интерес!</h3>
             <img src={accept} alt="" />
             <h4>Подпишитесь на наш официальный <br /> Telegram канал, чтобы ничего не пропустить.</h4>
@@ -19,7 +19,7 @@ const SuccessForm = () => {
                 <button>Подписаться</button>
             </form>
         </div>
-        <div className="footer">Лето 2023</div>
+        <div className="success_footer">Лето 2023</div>
     </div>
   )
 }
